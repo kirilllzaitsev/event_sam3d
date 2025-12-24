c05a9b7d24eb61de52e41df346fbbea66c381b3e
kirilllzaitsev <kzaitsev88@gmail.com>
2025-12-24 23:46:41 +0100

    add mu

---
+++ event_sam3d/img2event/model_utils.py
@@ -0,0 +1,46 @@
+import argparse
+
+import torch
+import yaml
+
+from event_sam3d.config import PROJ_DIR
+from event_sam3d.img2event.train import Trainer, build_model
+
+
+def load_esam3d(ckpt_name, device="cuda", ckpt_type="best"):
+    # best/latest
+    ckpt_dir = f"{PROJ_DIR}/checkpoints/{ckpt_name}"
+    cfg = argparse.Namespace(
+        **yaml.load(open(f"{ckpt_dir}/config.yml"), Loader=yaml.UnsafeLoader)
+    )
+    build_model_res = build_model(cfg, rank=0)
+    model, forward_args = (
+        build_model_res["model"].to(device),
+        build_model_res["forward_args"],
+    )
+    ckpt = torch.load(f"{ckpt_dir}/{ckpt_type}.pt", map_location="cpu")
+    model.load_state_dict(ckpt["model"])
+    for param in model.parameters():
+        param.requires_grad = False
+    model.eval()
+    trainer = Trainer(model, cfg)
+    return {
+        "cfg": cfg,
+        "model": model,
+        "trainer": trainer,
+        "forward_args": forward_args,
+    }
+
+
+def get_condition_embedder(pipe):
+    condition_embedder = [
+        x
+        for x in pipe.condition_embedders["ss_condition_embedder"].embedder_list
+        if all("event" in xx[0] for xx in x[1])
+    ]
+    assert len(condition_embedder) == 1 and len(condition_embedder[0]) == 2, len(
+        condition_embedder
+    )
+    # same encoder for full/cropped imgs
+    condition_embedder = condition_embedder[0][0]
+    return condition_embedder
