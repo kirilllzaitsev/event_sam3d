53b05dfd66b9c222b5c0a4fe6046f1969a181831
kirilllzaitsev <kzaitsev88@gmail.com>
2025-12-24 23:18:33 +0100

    add IEDataset

---
+++ event_sam3d/datasets/ie_dataset.py
@@ -0,0 +1,42 @@
+import numpy as np
+import torch
+
+from event_sam3d.utils.event_utils import VoxelGrid
+
+
+class IEDataset(torch.utils.data.Dataset):
+    def __init__(self, datasets, use_vg_event_repr=True):
+
+        self.datasets = datasets
+
+        self.dataset_lengths = [len(v) for k, v in datasets.items()]
+        self.cum_lengths = np.cumsum(self.dataset_lengths)
+
+        first_ds = list(datasets.values())[0]
+        self.use_vg_event_repr = use_vg_event_repr and not first_ds.use_vg_event_repr
+        self.hw = (first_ds.height, first_ds.width)
+        assert all((d.height, d.width) == self.hw for d in datasets.values()), [
+            (k, d.height, d.width) for k, d in datasets.items()
+        ]
+        if use_vg_event_repr:
+            self.vg = VoxelGrid(3, self.hw[0], self.hw[1])
+
+    def __len__(self):
+        return sum(self.dataset_lengths)
+
+    def __getitem__(self, idx):
+        # Find which dataset the index belongs to
+        dataset_idx = np.searchsorted(self.cum_lengths, idx, side="right")
+        if dataset_idx == 0:
+            sample_idx = idx
+        else:
+            sample_idx = idx - self.cum_lengths[dataset_idx - 1]
+
+        dataset_key = list(self.datasets.keys())[dataset_idx]
+        sample = self.datasets[dataset_key][sample_idx]
+
+        if self.use_vg_event_repr:
+            event_repr = self.vg.convert(event_dict=sample["events"])
+            sample["events"] = event_repr
+
+        return sample
