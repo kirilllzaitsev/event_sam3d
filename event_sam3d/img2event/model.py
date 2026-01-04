84f2a985836c914734110963c0276dfd0a5d38a4
kirilllzaitsev <kzaitsev88@gmail.com>
2026-01-04 22:50:54 +0100

    m:set hooks for fuser out

--- event_sam3d/img2event/model.py
+++ event_sam3d/img2event/model.py
@@ -1,3 +1,4 @@
+import functools
 from collections import defaultdict
 
 import torch
@@ -45,31 +46,29 @@ class TeacherStudent(nn.Module):
         self.s_embeds = defaultdict(list)
         self.t_embeds = defaultdict(list)
 
-        def get_hook(name, embeds):
-            def hook(module, input, output):
-                embeds[name].append(output)
-
-            return hook
-        def set_hooks(net, embeds, use_event):
-
-            condition_embedder = get_condition_embedder(net, use_event=use_event)
-            hooks = []
-
-            for i in self.block_idxs:
-                layer = fetch_module_by_name(condition_embedder, f"{t_blocks_name}.{i}")
-
-
-                hook = layer.register_forward_hook(get_hook(f"block_{i}", embeds=embeds))
-                hooks.append(hook)
-            return hooks
-
-        self.s_hooks = set_hooks(self.s, self.s_embeds, use_event=True)
-        layer = fetch_module_by_name(self.s, "condition_embedders.ss_condition_embedder.rgbe_fuser")
-        hook = layer.register_forward_hook(get_hook("rgbe_fuser", embeds=self.s_embeds))
-        layer = get_condition_embedder(self.t, use_event=False)
+        self.s_hooks = set_hooks(
+            self.s, self.s_embeds, use_event=True, block_idxs=self.block_idxs
+        )
+        self.t_hooks = set_hooks(
+            self.t, self.t_embeds, use_event=False, block_idxs=self.block_idxs
+        )
+        layer = self.s.condition_embedders["ss_condition_embedder"]
+        hook = layer.register_forward_hook(
+            get_hook(
+                "t_final_rgb_tokens",
+                embeds=self.s_embeds,
+                fn=functools.partial(dict_fn, k="rgb_image_tokens"),
+            )
+        )
         self.s_hooks.append(hook)
-        self.t_hooks = set_hooks(self.t, self.t_embeds, use_event=False)
-        hook = layer.register_forward_hook(get_hook("t_final_rgb_tokens", embeds=self.t_embeds))
+        layer = self.t.condition_embedders["ss_condition_embedder"]
+        hook = layer.register_forward_hook(
+            get_hook(
+                "t_final_rgb_tokens",
+                embeds=self.t_embeds,
+                fn=functools.partial(dict_fn, k="rgb_image_tokens"),
+            )
+        )
         self.t_hooks.append(hook)
 
     def forward(self, s_kwargs, t_kwargs=None, **kwargs):
@@ -82,3 +81,40 @@ class TeacherStudent(nn.Module):
                 t_pred = self.t(**t_kwargs, **kwargs)
             res.update({"t_pred": t_pred, "t_feats": self.t_embeds})
         return res
+
+
+def dict_fn(output, k, input=None):
+    return output[k]
+
+
+def get_hook(name, embeds, fn=None):
+    def hook(module, input, output):
+        if fn is not None:
+            output = fn(output)
+        embeds[name].append(output)
+
+    return hook
+
+
+def get_pre_hook(name, embeds, fn=None, kwarg_name=None):
+    def hook(module, input, kwargs):
+        if kwarg_name is None:
+            embeds[name].append(input)
+        else:
+            embeds[name].append(kwargs[kwarg_name][0])
+
+    return hook
+
+
+def set_hooks(net, embeds, use_event, block_idxs, t_blocks_name="backbone.blocks"):
+    from event_sam3d.img2event.model_utils import get_condition_embedder
+
+    condition_embedder = get_condition_embedder(net, use_event=use_event)
+    hooks = []
+
+    for i in block_idxs:
+        layer = fetch_module_by_name(condition_embedder, f"{t_blocks_name}.{i}")
+
+        hook = layer.register_forward_hook(get_hook(f"block_{i}", embeds=embeds))
+        hooks.append(hook)
+    return hooks
